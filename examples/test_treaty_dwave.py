900761cb4921953baad495d516fb49645f320112
ak-skwaa-mahawk <johnbcarrolljr@gmail.com>
2025-10-22 18:33:09 -0800

    Create examples/test_treaty_dwave.py

---
+++ examples/test_treaty_dwave.py
@@ -0,0 +1,9 @@
+from fpt_core.neutrosophic_transport import NeutrosophicTransport
+import pandas as pd
+
+nt = NeutrosophicTransport([0], [1, 2, 3, 4])
+treaty_data = pd.read_csv("data/treaty_texts.csv")["value"].values
+energy, obj, sample = nt.optimize_treaty_leap(treaty_data)
+print(f"Treaty optimized energy: {energy}")
+print(f"Neutrosophic scores: T={obj['T']:.4f}, I={obj['I']:.4f}, F={obj['F']:.4f}")
+print(f"Best sample: {sample}")
