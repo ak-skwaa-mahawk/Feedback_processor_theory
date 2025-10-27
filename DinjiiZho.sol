585d65bc22c87ec154c18221c10031a35cee17dc
ak-skwaa-mahawk <johnbcarrolljr@gmail.com>
2025-10-26 20:26:36 -0800

    Update DinjiiZho.sol
    git add recursive_simulation.py DinjiiZho.sol
    git commit -m "Simulated nested shard recursion with 3x3x8x8 structure (v0.4.7)"
    git push origin main

--- DinjiiZho.sol
+++ DinjiiZho.sol
@@ -4,12 +4,14 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract DinjiiZho is TribalCoinGTC, ERC721 {
-    mapping(uint256 => mapping(uint8 => string[8])) public shardGlyphs;  // 3 nodes, 3 sub-nodes, 8 shards
+    mapping(uint256 => mapping(uint8 => mapping(uint8 => string[8]))) public shardGlyphs;  // 3 nodes, 3 sub-nodes, 8 shards
     mapping(uint256 => uint8) public generation;
+    mapping(uint256 => mapping(uint8 => mapping(uint8 => string[8]))) public subShardGlyphs;  // Recursive layer
     uint256 public storyCount;
     mapping(address => bool) public isElder;
 
     event ShardMinted(uint256 indexed tokenId, uint8 node, uint8 subNode, string[8] shards, uint8 gen, uint timestamp);
+    event SubShardMinted(uint256 indexed tokenId, uint8 node, uint8 subNode, uint8 shardIdx, string[8] subShards);
 
     constructor(uint256 initialSupply) TribalCoinGTC(initialSupply) ERC721("Dinjii Zho", "DZHO") {
         storyCount = 0;
@@ -30,5 +32,13 @@ contract DinjiiZho is TribalCoinGTC, ERC721 {
         generation[storyCount] = gen;
         emit ShardMinted(storyCount, node, subNode, shards, gen, block.timestamp);
         storyCount++;
+    }
+
+    function mintSubShardGlyphs(uint256 tokenId, uint8 node, uint8 subNode, uint8 shardIdx, string[8] memory subShards) public onlyVerified {
+        require(isElder[msg.sender], "Only elders mint.");
+        require(ownerOf(tokenId) == msg.sender, "Not owner");
+        require(shardIdx < 8, "Invalid shard index");
+        subShardGlyphs[tokenId][node * 3 + subNode][shardIdx] = subShards;
+        emit SubShardMinted(tokenId, node, subNode, shardIdx, subShards);
     }
 }
