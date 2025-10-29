07b37782c63595d221107a9cdd34ad7c9887c9b1
ak-skwaa-mahawk <johnbcarrolljr@gmail.com>
2025-10-28 16:00:20 -0800

    Create glyph_generator.py

---
+++ glyph_generator.py
@@ -0,0 +1,7 @@
+class GlyphGenerator:
+    """AGŁL child glyph creation"""
+    
+    def generate_child(self, scrape) -> str:
+        """Generate a glyph based on scrape"""
+        # TODO: implement glyph generation
+        return "glyph_placeholder"
