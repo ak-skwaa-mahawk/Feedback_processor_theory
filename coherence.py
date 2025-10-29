a0c9fabec674697940576bb1c15513df0df73aaf
ak-skwaa-mahawk <johnbcarrolljr@gmail.com>
2025-10-28 16:00:43 -0800

    Create coherence.py

---
+++ coherence.py
@@ -0,0 +1,7 @@
+class Coherence:
+    """Compute coherence between scrapes"""
+    
+    def compute(self, scrape1, scrape2) -> float:
+        """Return coherence score [0-1]"""
+        # TODO: implement vector/entropy/temporal alignment
+        return 0.0
