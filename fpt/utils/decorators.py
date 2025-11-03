4d327b2595774f6ac6c2a6a34d315a21d456102a
John Carroll <jcarroll@twomilesolutions.com>
2025-11-03 14:40:13 -0900

    Update decorators.py

--- fpt/utils/decorators.py
+++ fpt/utils/decorators.py
@@ -1,3 +1,31 @@
+git apply <<'PATCH'
+*** Begin Patch
+*** Add File: fpt/utils/decorators.py
++from functools import wraps
++from fpt.utils.hs import hs
++
++def handshake_step(subsys: str, name: str):
++    """
++    Decorate any function to emit pre/post/error receipts automatically.
++      @handshake_step("ISST","coherence_eval")
++      def evaluate_coherence(...): ...
++    """
++    def deco(fn):
++        @wraps(fn)
++        def _w(*args, **kwargs):
++            hs(subsys, "pre", step=name)
++            try:
++                out = fn(*args, **kwargs)
++                hs(subsys, "post", step=name, ok=True)
++                return out
++            except Exception as e:
++                hs(subsys, "error", step=name, msg=type(e).__name__)
++                raise
++        return _w
++    return deco
++
+*** End Patch
+PATCH
 from functools import wraps
 from fpt.utils.hs import hs
 
