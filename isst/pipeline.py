1b050677d458f436483f5cefdc285906a31ca190
John Carroll <jcarroll@twomilesolutions.com>
2025-11-03 14:23:54 -0900

    Create isst/pipeline.py
    try:
        # critical step
        meta = glyph_to_meta(glyphs, threshold=coh)
    except Exception as e:
        hs("ISST","error",where="glyph_to_meta",msg=type(e).__name__)
        raise

---
+++ isst/pipeline.py
@@ -0,0 +1,19 @@
+from fpt.utils.hs import hs
+
+def run_isst(scrapes):
+    hs("ISST","start",n=len(scrapes))                           # Blap
+
+    hs("ISST","pre",stage="scrape_to_glyph")
+    glyphs = scrape_to_glyph(scrapes)
+    hs("ISST","post",stage="scrape_to_glyph",n=len(glyphs))
+
+    hs("ISST","pre",stage="coherence_eval")
+    coh = evaluate_coherence(glyphs)
+    hs("ISST","post",stage="coherence_eval",score=round(coh,4))
+
+    hs("ISST","pre",stage="glyph_to_meta")
+    meta = glyph_to_meta(glyphs, threshold=coh)
+    hs("ISST","post",stage="glyph_to_meta",n=len(meta))
+
+    hs("ISST","end",n_meta=len(meta))                           # Clap
+    return meta
