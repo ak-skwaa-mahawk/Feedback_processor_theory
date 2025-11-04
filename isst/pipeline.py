2f32f92184c0e7e473cb587f7ba0704b6039855b
John Carroll <jcarroll@twomilesolutions.com>
2025-11-03 15:49:59 -0900

    Update pipeline.py

--- isst/pipeline.py
+++ isst/pipeline.py
@@ -1,4 +1,10 @@
 from fpt.utils.hs import hs
+hs("ISST","start",n=len(scrapes))
+hs("ISST","pre",stage="scrape_to_glyph"); ... ; hs("ISST","post",stage="scrape_to_glyph",n=len(glyphs))
+hs("ISST","pre",stage="coherence_eval"); ... ; hs("ISST","post",stage="coherence_eval",score=round(coh,4))
+hs("ISST","pre",stage="glyph_to_meta"); ... ; hs("ISST","post",stage="glyph_to_meta",n=len(meta))
+hs("ISST","end",n_meta=len(meta))
+from fpt.utils.hs import hs
 
 def run_isst(scrapes):
     hs("ISST","start",n=len(scrapes))                           # Blap
