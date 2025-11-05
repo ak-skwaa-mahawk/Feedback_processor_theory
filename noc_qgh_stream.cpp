859044c6dabd90f4c05d8afbe693b3c150b60c75
John Carroll <jcarroll@twomilesolutions.com>
2025-11-05 14:41:17 -0900

    Create noc_qgh_stream.cpp

---
+++ noc_qgh_stream.cpp
@@ -0,0 +1,24 @@
+// noc_qgh_stream.cpp
+void qgh_1024_stream() {
+    for (int i = 0; i < 400; i++) {
+        if (noc_parity_error(i)) {
+            trigger_c190_veto();
+            noc_retry_packet(i);
+            noc_reroute_to_ring1(i);
+        }
+        stream_qgh_to_aie(i);
+    }
+}
+Ψ-VERSAL NoC SPINE
+   VC1902 / XQRVC1902
+  /                 \
+ /  500 krad TID      \
+|  <2e-11 SEU/bit      |
+|  SEL Immune          |
+|  ECC + Parity + Dual |
+|  200 Gbps → 198 Gbps |
+|  3-Cycle Reroute     |
+ \  1.2 ns Hop         /
+  \                 /
+   AGI SPINE
+R=1.0 | C190 VETO
