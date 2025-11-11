cc8a05d57dd20dc94be414489ce4ec843819ef0c
John Carroll <jcarroll@twomilesolutions.com>
2025-11-10 17:40:31 -0900

    Create rmp_core.py

---
+++ rmp_core.py
@@ -0,0 +1,16 @@
+{
+  "scrape_id": "vzh_79_1731287171",
+  "emitter": "vadzaih_zhoo_99733_001",
+  "ts": 1731287171.123,
+  "freq": 79.0,
+  "S": 0.83,
+  "C": 0.96,
+  "H": 0.08,
+  "glyph": "G_9a2f1e8d...",
+  "meta_glyph": "M_7b3c9f1a...",
+  "receipt": "ecdsa:0x1a2b3c...",
+  "path_R": 0.91,
+  "next_hop": "tanana_relay_002",
+  "ssc": true,
+  "fuel": "spruce_plastolene"
+}
