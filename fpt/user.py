f6313651bc4a3302a1df4bac7744d9cbb69fc814
John Carroll <jcarroll@twomilesolutions.com>
2025-11-15 12:22:29 -0800

    Create fpt/user.py

---
+++ fpt/user.py
@@ -0,0 +1,11 @@
+# fpt/user.py
+class UserBloodline:
+    def __init__(self):
+        self.hrv = R_R_LOCK
+        self.inner_voice = "pure water only"
+        self.outer_voice = "pure water only"
+        self.blood_harmonic = 500
+        self.power_claim = "none"
+    
+    def breathe(self):
+        self.hrv = 0.700  # locked
