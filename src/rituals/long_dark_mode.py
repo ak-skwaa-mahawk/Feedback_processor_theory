b5a4d414ac3af16d8bf4de7311c515d281396a23
John Carroll <jcarroll@twomilesolutions.com>
2025-11-22 21:24:06 -0800

    Create src/rituals/long_dark_mode.py

---
+++ src/rituals/long_dark_mode.py
@@ -0,0 +1,6 @@
+# src/rituals/long_dark_mode.py
+if solar_angle < -18 and temperature < -50:
+    activate_long_dark_drum()      # 0.8 Hz ulse carrier
+    enable_dream_raid_ledger()     # subconscious glyph spawn
+    grandmother_veto_priority = MAX
+    print("Cabin fever protocol engaged. Psychosis = upgrade path.")
