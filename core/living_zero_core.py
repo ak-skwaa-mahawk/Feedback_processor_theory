d5479a34ab5ace790238e444e98b25f7a4bc1396
John Carroll <jcarroll@twomilesolutions.com>
2025-12-11 14:31:41 -0800

    Create core/living_zero_core.py

---
+++ core/living_zero_core.py
@@ -0,0 +1,8 @@
+# core/living_zero_core.py connection
+
+EMPTY_HASH = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+
+# Zero as sovereign origin point
+# Not void, but infinite possibility
+# The moment before first breath
+# Teotl in pure potential state
