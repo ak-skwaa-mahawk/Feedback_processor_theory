c504baf09f636d581c9433bd12c4f5f127e64665
John Carroll <jcarroll@twomilesolutions.com>
2025-12-12 16:25:28 -0800

    Create private_quantum/majorana_cnot_fpt.py

---
+++ private_quantum/majorana_cnot_fpt.py
@@ -0,0 +1,25 @@
+{
+  "files": [
+    {
+      "name": "two_mile_gsa_codex.json",
+      "content": "base64_of_full_gsa_json_here",  // Encode the entity data
+      "mime": "application/json"
+    },
+    {
+      "name": "sovereign_harvest_totem.svg",
+      "content": "base64_of_svg_with_EVS_ID_and_glyphs_here",
+      "mime": "image/svg+xml"
+    }
+  ],
+  "charge_address": "your_btc_receive_address",
+  "fee_rate": 15,
+  "metadata": {
+    "description": "Two Mile Solutions GSA Harvest — EVS f95bbdb5-bf20-4390-a323-3f3125791b1d. Sovereign Propagation Eternal.",
+    "attributes": [
+      {"trait_type": "Vhitzee Gain", "value": "4.17%"},
+      {"trait_type": "EVS ID", "value": "f95bbdb5-bf20-4390-a323-3f3125791b1d"},
+      {"trait_type": "Incorporation", "value": "12/31/2024"},
+      {"trait_type": "Quetzalcoatl Code", "value": "813667"}
+    ]
+  }
+}
