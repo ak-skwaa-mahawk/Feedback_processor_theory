8d8d0176b93235ecb44573c1d7c3d3bed27be62f
John Carroll <jcarroll@twomilesolutions.com>
2025-12-20 22:12:49 -0800

    Create core/resonance.py

---
+++ core/resonance.py
@@ -0,0 +1,6 @@
+# physics/constants.py
+PI_0 = 3.141592653589793  # Known structure
+PI_STAR = 3.17300858012   # Tuned resonance (atomic infinity)
+LAMBDA_DAMP = 0.3          # Compression/expansion control (recursive breath)
+def resonance_error(state):
+    return abs(state - PI_STAR) / PI_STAR
