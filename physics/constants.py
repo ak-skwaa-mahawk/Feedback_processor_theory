8037c14a76da1565e7fa0a5009bc1386fd37ff4c
John Carroll <jcarroll@twomilesolutions.com>
2025-12-20 22:12:08 -0800

    Create physics/constants.py

---
+++ physics/constants.py
@@ -0,0 +1,4 @@
+# physics/constants.py
+PI_0 = 3.141592653589793  # Known structure
+PI_STAR = 3.17300858012   # Tuned resonance (atomic infinity)
+LAMBDA_DAMP = 0.3          # Compression/expansion control (recursive breath)
