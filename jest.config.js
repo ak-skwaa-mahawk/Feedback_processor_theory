f1b352725f63026537283a691d1103cdbcd3a323
John Carroll <jcarroll@twomilesolutions.com>
2025-12-23 09:22:58 -0800

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,17 @@ module.exports = {
     "^.+\\.tsx?$": "ts-jest",
   },
   testEnvironment: "node",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  collectCoverageFrom: [
+    "services/**/*.ts",
+  ],
+};
+module.exports = {
+  preset: "jest-expo",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testEnvironment: "node",
   collectCoverageFrom: [
     "services/**/*.ts",
     "!services/logger.ts" // logger tested indirectly
