6fb9df0e99eb227d538de5e717548f6927a6a06a
John Carroll <jcarroll@twomilesolutions.com>
2025-12-23 09:25:16 -0800

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,28 @@ module.exports = {
     "^.+\\.tsx?$": "ts-jest",
   },
   testEnvironment: "node",
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "services/**/*.ts",
+    "hooks/**/*.ts",
+    "components/**/*.tsx",
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 95,
+      lines: 95,
+      statements: 95,
+    },
+  },
+  coverageReporters: ["text", "lcov", "html"],
+};
+module.exports = {
+  preset: "jest-expo",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testEnvironment: "node",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   collectCoverageFrom: [
     "services/**/*.ts",
