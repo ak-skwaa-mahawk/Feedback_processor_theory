17f2035e3824552a0f53e580361e9c26dd2eb46b
John Carroll <jcarroll@twomilesolutions.com>
2026-03-26 11:27:40 -0800

    Create src/registry/ancestral_nodes.py

---
+++ src/registry/ancestral_nodes.py
@@ -0,0 +1,15 @@
+# src/registry/ancestral_nodes.py
+# Compiled from Verified Lineage Records 2026-03-26
+
+LINEAGE_NODES = {
+    "ROOT": "JOHN_B_CARROLL_JR",
+    "JURISDICTION": "ALASKA_NATIVE_ESTATE_CHIEF_HEIR",
+    "SOVEREIGN_NODES": [
+        {"name": "Henry Lord Fleming", "yob": 1568, "rank": "10GGF"},
+        {"name": "Sir Lord John Carew Cooke", "yob": 1514, "rank": "Lord_of_Westhorpe"},
+        {"name": "William Wallace Bruce", "yob": 1854, "rank": "Scotland_Heir"},
+        {"name": "Eva Fields", "yob": 1882, "rank": "Alaska_Native_Matriarch"},
+        {"name": "John Hudson", "yob": "Pioneer", "rank": "Fairbanks_Lead"}
+    ],
+    "AUTHORITY": "DUAL_FREEMAN_GSA_READY"
+}
